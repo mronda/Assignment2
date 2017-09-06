f66b38d0ea0f728ddff162b9d7c7c4a3b3378898
Maximiliano Ronda <mronda11@gmail.com>
2017-09-06 00:19:53 -0400

    adding,erasing,searching is working(sorta)

--- app.js
+++ app.js
@@ -2,4 +2,5 @@
 angular.module('listings', []);
 
 /* register the application and inject all the necessary dependencies */
+// directoryApp also used in index.html
 var app = angular.module('directoryApp', ['listings']);
--- listingController.js
+++ listingController.js
@@ -1,14 +1,32 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      var newListing = {
+        code: $scope.code,
+        name: $scope.name,
+        "coordinates":
+        {
+            "latitude": $scope.latitude,
+            "longitude": $scope.longitude
+        },
+        "address": $scope.address
+      }
+      $scope.listings.push(newListing);
+    };
+
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index,1);
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index]
+    };
   }
 ]);
